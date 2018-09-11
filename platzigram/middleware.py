737b464f55a1cef098f22b0c7557507628c32a91
Alejandro Duque C <ducode@outlook.com>
2018-09-11 12:04:44 -0500

    middleware and sign up views implemented

---
+++ platzigram/middleware.py
@@ -0,0 +1,19 @@
+"""middleware to ensure bio information"""
+#django
+from django.shortcuts import redirect
+from django.urls import reverse
+class ProfileCompletionMiddleware:
+    """ Profile completion middleware ensure that user have any
+    bio information"""
+    def __init__(self,get_response):
+        self.get_response=get_response
+    def __call__(self,request):
+        """code to be executed for each request before the view is called"""
+        if not request.user.is_anonymous:
+            profile=request.user.profile
+            if not profile.picture or not profile.biography:
+                #except some urls
+                if request.path not in [reverse('update_profile'),reverse('logout')]:
+                    return redirect('update_profile')
+        response=self.get_response(request)
+        return response
--- users/views.py
+++ users/views.py
@@ -3,6 +3,14 @@
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+#Exeption
+from django.db.utils import IntegrityError
+
+def update_profile(request):
+    return render(request,'users/update_profile.html')
 
 def login_view(request):
     """login view"""
@@ -22,3 +30,26 @@ def logout_view(request):
     """logout view"""
     logout(request)
     return redirect('login')
+
+def signup(request):
+    """sigin up view"""
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        password_confimation=request.POST['password_confimation']
+       
+        if password_confimation != password:
+            return render(request,'users/signup.html',{'error':'please check password try to write the same in the two fields'})
+        try:
+            user = User.objects.create_user(username=username,password=password)
+        except IntegrityError:
+            return render(request,'users/signup.html',{'error':'Username is already in use'})
+        user.firstname=request.POST['firstname']
+        user.lastname=request.POST['lastname']
+        user.email=request.POST['email']
+        user.save()
+        profile=Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request,'users/signup.html')
+
